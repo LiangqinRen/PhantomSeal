4f2a3841ce51adb3178946edb55b4b3b3068597f
LiangqinRen <liangqinren@gmail.com>
2025-06-24 21:45:17 -0500

    feat(log): enable clean script to remove empty folder

--- scripts/clean_debug_log.sh
+++ scripts/clean_debug_log.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-find log/* -maxdepth 1 -type d -name '*_debug' -exec rm -rf {} +
+LOGS_DIR="logs"
+
+for date_dir in "$LOGS_DIR"/*; do
+  [ -d "$date_dir" ] || continue
+
+  for time_dir in "$date_dir"/*; do
+    if [[ -d "$time_dir" && "$time_dir" == *_debug ]]; then
+      echo "Deleting debug folder: $time_dir"
+      rm -rf "$time_dir"
+    fi
+  done
+
+  if [ -z "$(ls -A "$date_dir")" ]; then
+    echo "Deleting empty date folder: $date_dir"
+    rmdir "$date_dir"
+  fi
+done
