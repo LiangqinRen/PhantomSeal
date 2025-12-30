07a4a25859bd3a0d0c35d7828d37e121ae9456c5
LiangqinRen <liangqinren@gmail.com>
2025-12-29 20:53:04 -0600

    chore: add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+ROOT=$(dirname $(dirname $(realpath "$0")))
+
+echo -e "\033[1;34mPreparing dependencies for HifiFace...\033[0m"
+(
+    cd $ROOT/third_party/HifiFace || exit
+    git clone https://github.com/sicxu/Deep3DFaceRecon_pytorch && git clone https://github.com/NVlabs/nvdiffrast && git clone https://github.com/deepinsight/insightface.git
+    cp -r insightface/recognition/arcface_torch/ Deep3DFaceRecon_pytorch/models/
+    cp -r insightface/recognition/arcface_torch/ ./model/
+    rm -rf insightface
+    cp -rf 3DMM/* Deep3DFaceRecon_pytorch
+    mv Deep3DFaceRecon_pytorch model/
+    rm -rf 3DMM
+    rm -rf nvdiffrast
+)
+
+echo -e "\033[1;34mDownloading pre-trained models and datasets...\033[0m"
+(
+    gdown 1NwZW8iV9wBPKUQFzZ18EfgDhm8HcyWX4 -O data.tar
+    tar -xf data.tar
+    rm data.tar
+)
+
+echo -e "\033[1;34mDownloading pre-trained models...\033[0m"
+(
+    declare -A FILES=(
+        ["third_party/SimSwap/checkpoints/people/latest_net_D1.pth"]="1J4hiXljlqNtVC8bVqItT2Px1JoR2hI1M"
+        ["third_party/SimSwap/checkpoints/people/latest_net_D2.pth"]="1_yGkDuL0ZcOg6aqcNTZUkLWg--48IbeU"
+        ["third_party/SimSwap/checkpoints/people/latest_net_G.pth"]="1VlJCRwtUP0xR5PKimJ8wDsffwzTGsMVI"
+        ["third_party/SimSwap/arcface_model/arcface_checkpoint.tar"]="1601KmNPWsULuGgyKnwoFKJxHF7vPcwMO"
+
+        ["third_party/FaceShifter/ModelC/saved_models/G_latest.pth"]="137YW1XEkpacUD35lpl2rhWkH-Qbasukn"
+        ["third_party/FaceShifter/ModelC/face_modules/model_ir_se50.pth"]="1DIXjw81VQS2lce21HOHIcHqUC8g5rrXH"
+
+        ["third_party/HifiFace/ms1mv3_arcface_r100_fp16_backbone.pth"]="15K-wd8-DNyz3Vc_VT_G0rS0_txK6IVCY"
+        ["third_party/HifiFace/model/Deep3DFaceRecon_pytorch/checkpoints/epoch_20.pth"]="1A9CrnzqiS6xt6P-7NpPPm9B6Wv3JEU0k"
+        ["third_party/HifiFace/model/Deep3DFaceRecon_pytorch/BFM/01_MorphableModel.mat"]="1vEeFfFNBJuM88aAKAt4gZleI8bIJUJlM"
+        ["third_party/HifiFace/checkpoints/hififace_opensouce_299999.ckpt"]="1jc8LyfRPoDI6WTnJDH0jkpl9cShc9tMh"
+
+        ["third_party/DiffFace/checkpoints/Model.pt"]="1kYnnctk4L4Z9TbvNCvCHEtJeO0WT_UHc"
+        ["third_party/DiffFace/checkpoints/GazeEstimator.pt"]="12AH0_iT23hMUBQAiRU7oq0psnK7pvAUq"
+        ["third_party/DiffFace/checkpoints/FaceParser.pth"]="1HZF2Z1aK-dxQ92oEhfuRbboGBOtE2mve"
+        ["third_party/DiffFace/checkpoints/Arcface_model_only.tar"]="1qiAiF8g1lKD59VWB2vA50mGE2HDkflgZ"
+        ["third_party/HifiFace/model/Deep3DFaceRecon_pytorch/BFM/Exp_Pca.bin"]="1Wwjp_3ZcvwUyJZLfdlzHcmB5DPZ03roI"
+    )
+
+    for rel in "${!FILES[@]}"; do
+        id="${FILES[$rel]}"
+        dest="$ROOT/$rel"
+
+        mkdir -p "$(dirname "$dest")"
+
+        if [[ -f "$dest" ]]; then
+            echo "[Skip] $rel exists"
+            continue
+        fi
+
+        echo "[Download] $rel"
+        gdown "https://drive.google.com/uc?id=$id" -O "$dest"
+    done
+)
+
+echo -e "\033[1;34mEnjoy! 🥳\033[0m"
